90c7ea5316d8ba9770d7c7e79d2e6a0586a609bc
Katasonov Vladyslav <cpud47@gmail.com>
2019-08-16 00:03:40 +0300

    Added distributions precomputation.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 extern crate rand;
 extern crate rayon;
 
-use rand::Rng;
-use rayon::prelude::*;
-
-use rand::prelude::ThreadRng;
 use std::collections::HashMap;
 use std::time::SystemTime;
+
+use rand::prelude::ThreadRng;
+use rand::Rng;
+use rayon::prelude::*;
 
 #[derive(Clone, Copy)]
 struct Weapon {
@@ -64,18 +64,20 @@ impl Enemy {
         (passed as f32 * weapon.health_multiplier) as u32
     }
 
-    fn kill(&self, weapon: &Weapon) -> i32 {
+    fn kill(&self, distribution_cache: &DiceDistributionCache, weapon: &Weapon) -> i32 {
+        let distribution = distribution_cache.distribution_for(weapon.dice_count as usize);
         let mut steps: i32 = 0;
-        let mut damage: f32 = 0.0;
+        let mut average_damage: f32 = 0.0;
         for res in weapon.dice_count..=weapon.dice_count * 6 {
-            damage +=
-                self.get_damage(res as i32, weapon) * get_roll_posibility(res, weapon.dice_count);
+            let probability = distribution.probability_of(res as usize);
+            let damage = self.get_damage(res as i32, weapon);
+            average_damage += damage as f32 * probability;
         }
 
-        if damage.abs() < 0.1 {
+        if average_damage.abs() < 0.1 {
             steps = -400
         } else {
-            steps = (self.health as f32 / damage).round() as i32;
+            steps = (self.health as f32 / average_damage).round() as i32;
 
             if steps > 300 {
                 steps = -400
@@ -113,33 +115,63 @@ impl Sample {
     }
 }
 
+#[derive(Debug, Clone)]
 struct DiceDistribution {
-    chances: Vec<Vec<f32>>,
+    dice_count: usize,
+    chances: Vec<f32>,
+}
+
+#[derive(Debug, Clone)]
+struct DiceDistributionCache {
+    distributions: Vec<DiceDistribution>,
 }
 
 impl DiceDistribution {
-    fn precompute() -> DiceDistribution {
-        let chances: Vec<Vec<f32>>;
-
+    fn precomputed_for(dice_count: usize) -> Self {
         let mut random = rand::thread_rng();
 
-        for dice_count in 1..26 {
-            for _ in 0..10000 {
-                let roll = (0..dice_count).map(|_| random.gen_range(1, 7)).sum();
-            }
+        let total_count = 10000;
+
+        // At i'th index, we store count of rolls with sum equal to i + dice_count
+        let mut roll_count: Vec<usize> = (dice_count..=dice_count * 6).map(|_| 0).collect();
+        for _ in 0..total_count {
+            let roll: usize = (0..dice_count).map(|_| random.gen_range(1, 7)).sum();
+            roll_count[roll - dice_count] += 1;
         }
 
-        DiceDistribution { chances }
+        Self {
+            dice_count,
+            chances: roll_count
+                .into_iter()
+                .map(|x| x as f32 / total_count as f32)
+                .collect(),
+        }
+    }
+
+    fn probability_of(&self, roll: usize) -> f32 {
+        self.chances[roll - self.dice_count]
     }
 }
 
-fn get_roll_posibility(roll: u32, count: u32) -> f32 {
-    unimplemented!()
+impl DiceDistributionCache {
+    fn precomputed(max_dice_count: usize) -> Self {
+        Self {
+            distributions: (1..=max_dice_count)
+                .map(|dc| DiceDistribution::precomputed_for(dc))
+                .collect(),
+        }
+    }
+
+    fn distribution_for(&self, dice_count: usize) -> &DiceDistribution {
+        &self.distributions[dice_count - 1]
+    }
 }
 
 fn main() {
+    let distributions_cache = DiceDistributionCache::precomputed(26);
     let perfect_json = r#"{"Дробовик":{"Грант":9,"Титан":60,"Ховер":-400,"Громила":15,"Ремонтник":3,"Берсерк":15,"Гладиатор":30,"Банши":12,"Монстр":-400,"Надзиратель":6,"Брак":3,"Хаунд":21,"Турель":6,"Зомби":3,"Шмель":-400,"Дропер":-400,"Мясо":3,"Тактик":6,"Процессор":3,"Колобок":20},"Гвоздевик":{"Грант":7,"Титан":25,"Ховер":60,"Громила":10,"Ремонтник":1,"Берсерк":6,"Гладиатор":17,"Банши":5,"Монстр":40,"Надзиратель":4,"Брак":1,"Хаунд":13,"Турель":4,"Зомби":1,"Шмель":30,"Дропер":35,"Мясо":2,"Тактик":2,"Процессор":3,"Колобок":10},"Гипербластер":{"Грант":30,"Титан":80,"Ховер":220,"Громила":40,"Ремонтник":4,"Берсерк":18,"Гладиатор":60,"Банши":24,"Монстр":140,"Надзиратель":12,"Брак":2,"Хаунд":48,"Турель":12,"Зомби":2,"Шмель":125,"Дропер":110,"Мясо":4,"Тактик":8,"Процессор":12,"Колобок":50},"Клиповик":{"Грант":25,"Титан":0,"Ховер":0,"Громила":35,"Ремонтник":2,"Берсерк":12,"Гладиатор":57,"Банши":18,"Монстр":0,"Надзиратель":8,"Брак":2,"Хаунд":43,"Турель":8,"Зомби":1,"Шмель":0,"Дропер":0,"Мясо":3,"Тактик":5,"Процессор":8,"Колобок":20},"Бластер":{"Грант":-400,"Титан":-400,"Ховер":-400,"Громила":-400,"Ремонтник":5,"Берсерк":30,"Гладиатор":-400,"Банши":-400,"Монстр":-400,"Надзиратель":-400,"Брак":4,"Хаунд":-400,"Турель":-400,"Зомби":2,"Шмель":-400,"Дропер":-400,"Мясо":7,"Тактик":12,"Процессор":20,"Колобок":-400}}"#;
-    let weapons_json = r#"["Бластер","Клиповик","Дробовик","Гипербластер","Гвоздевик"]"#;
+    let weapons_json =
+        r#"["Бластер","Клиповик","Дробовик","Гипербластер","Гвоздевик"]"#;
     let enemies_json = r#"["Зомби","Брак","Мясо","Тактик","Турель","Надзиратель","Берсерк","Грант","Процессор","Громила","Банши","Гладиатор","Хаунд","Титан","Монстр","Ховер","Ремонтник","Колобок","Шмель","Дропер"]"#;
 
     let weapon_types_vec: Vec<String> = serde_json::from_str(weapons_json).unwrap();
@@ -200,7 +232,7 @@ fn main() {
             for (weapon_type, weapon) in sample.weapons.iter().enumerate() {
                 for (enemy_type, enemy) in sample.enemies.iter().enumerate() {
                     let current_enemy: Enemy = enemy.clone();
-                    let current_time_to_kill = current_enemy.kill(&weapon);
+                    let current_time_to_kill = current_enemy.kill(&distributions_cache, &weapon);
 
                     let perfect_time_to_kill = *perfect
                         .get(&(weapon_type as u32, enemy_type as u32))
